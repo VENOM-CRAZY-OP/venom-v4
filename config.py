73a948aa2a6fbea7521d20c5aaca9868af7a44a2
ankit192727 <148629450+ankit192727@users.noreply.github.com>
2024-08-31 20:01:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7500648513:AAGV6SOdH2QJjbXRDqdt7zPglT_6S6TewYY")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("ddoshelper1")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("930577300")
 
 
 
