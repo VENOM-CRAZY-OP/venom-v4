de63a018efd415e4f32ff413474b2f63a65b2d0f
aftabsaiyad <89257519+aftabsaiyad@users.noreply.github.com>
2024-09-01 16:49:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7326959611:AAHkdmykg5c7y_PXBAvMaBdNccKwi4tQKuo")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7091187275")
 
 
 
