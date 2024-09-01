08eae9ce8680a7964f3d46aad0f8aadafe336e8e
Theghost6838 <123843853+Theghost6838@users.noreply.github.com>
2024-09-01 10:54:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7451198481:AAGkCx-z5nAXJ2-jn8pgJVpUHCXrP6qbT4Y")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Samahi18_09")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5870168842")
 
 
 
