076fec7692a823311e9ac0df138c112c631dae5f
Obitovm <bgmidark84@gmail.com>
2024-09-02 18:17:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7535445247:AAFg3PIkxt-aMtIlAYUKp7UPD24KGHquwKM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("OBITOxVM")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1655261425")
 
 
 
