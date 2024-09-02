01a072cce4463583e0d31c9ea681f8205c174387
Suryanmsh <dragler2005ffindia3@gmail.com>
2024-09-02 14:52:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7379831659:AAHd1N4hI6UYdGg-mMYvbp3Uyyooqc34dhE")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("suryansh_27s")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5576100909")
 
 
 
