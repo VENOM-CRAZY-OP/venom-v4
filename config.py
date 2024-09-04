aa07a5b3379a21863241b5c2f90795bd7d070d81
Jitendra780 <jitendrakunwar19189@gmail.com>
2024-09-05 02:16:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7470728046:AAGVB4ZikCF_A9wlsdJqhl5EMYj0bb5mW0A")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Itz_BDG")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6066597118")
 
 
 
