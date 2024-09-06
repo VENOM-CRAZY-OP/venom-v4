2238dbe73d673d5ffde205deed68db5d5caa722a
sahdeepu619 <sahdeepu619@gmail.com>
2024-09-07 01:01:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7463396958:AAG39RCyARIWu6rjVPyr8sYMA22U4rrxpMs")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Itz_BDG")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6066597118")
 
 
 
