3db4733406a9214ca9de3b4c3c95903cfa7260fe
Aftabsaiya <aftabsaiyad00@gmail.com>
2024-09-06 09:54:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7202604495:AAGeoB6YL5mHQ2hCoRXEH2bmMRZGg3sXFDM")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Blackhardff")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1667632119")
 
 
 
