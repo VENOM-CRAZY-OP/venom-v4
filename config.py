d518c273777fa93072a866fc931fb95927d9e810
Mdsabir1919 <mdsabir48025@gmail.com>
2024-09-06 14:28:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7516266155:AAHiyt4kuW0nMitwwLcJMDjGo-rkHmN6WK4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Baba tillu")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5976088083")
 
 
 
