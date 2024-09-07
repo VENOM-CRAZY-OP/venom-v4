1b755bb5cb0be1ccc5765aa2c3bb6ef10eb59b69
ddosboth <dosd31086@gmail.com>
2024-09-07 09:27:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7222424089:AAGAl-DWOeKOXI0uQV4QvGwkfDnq1N3R31Y")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("@RAMGAMINGTZ")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1819374004")
 
 
 
