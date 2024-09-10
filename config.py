a975b5f670dc2cd0d56e0c36b59e19d387387cfa
Shivamqqte <vishalsainirdsss@gmail.com>
2024-09-10 20:20:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7379990663:AAHbrg7dkuOqF_H-M0LhdeNjrnbIYjJjQOQ")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Vishalsaini1153")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6797919326")
 
 
 
