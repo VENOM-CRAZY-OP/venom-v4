00665ae6c6c0a99761a0e8d817a726c4911c28e1
VENOM-CRAZY-OP <cyberecurityind@gmail.com>
2024-09-23 16:18:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("venomXcrazy")
